035c2ef1e1bb8226391e9a06afad6ce1ceaddd30
Felix Tristram <felix.tristram@googlemail.com>
2014-08-05 11:56:50 +0200

    Added a file reader/writer for the generated URIKeyMap for the AnchorFileGenerator.

--- AnchorParser/src/AnchorMapGenerator.java
+++ AnchorParser/src/AnchorMapGenerator.java
@@ -1,9 +1,51 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
+import java.util.LinkedList;
+
 
 public class AnchorMapGenerator {
 
-	public boolean createMapsFromAchorFile(String string) {
-		// TODO Auto-generated method stub
+	public static HashMap<String, LinkedList<String>> generateURIKeyMap(String fileName) throws FileNotFoundException {
 		
+		AnchorFileReader anchorReader = new AnchorFileReader(fileName);
+		HashMap<String, LinkedList<String>> URIKeyMap = new HashMap<String, LinkedList<String>>();
+		String[] triplet;
+		
+		while((triplet = anchorReader.getTriplet()) != null){
+			LinkedList<String> foundList;
+			if((foundList = URIKeyMap.get(triplet[1])) == null){ // key not found, create new list entry
+				LinkedList<String> newList = new LinkedList<String>();
+				newList.add(triplet[0]);
+				URIKeyMap.put(triplet[1], newList);
+			}else{ // key found, add string to list
+				//TODO: Do I have to make sure there are no double entries?
+				foundList.add(triplet[0]);
+			}
+		}
+		
+		return URIKeyMap;
+	}
+
+	public static void saveURIKeyMapToFile(HashMap<String, LinkedList<String>> uriKeyMap, String fileName) throws IOException{
+		ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(fileName));
+		out.writeObject(uriKeyMap);
+		out.close();
+	}
+
+	@SuppressWarnings("unchecked")
+	public static HashMap<String, LinkedList<String>> loadURIKeyMapFromFile(String fileName) throws IOException, ClassNotFoundException {
+		FileInputStream fileInputStream = new FileInputStream(fileName);
+		ObjectInputStream objectReader = new ObjectInputStream(fileInputStream);
+		HashMap<String, LinkedList<String>> uriKeyMap = new HashMap<String, LinkedList<String>>();
+		uriKeyMap = (HashMap<String, LinkedList<String>>) objectReader.readObject(); 
+		objectReader.close();
+		fileInputStream.close();
+		return uriKeyMap;
 	}
 	
 }
--- AnchorParser/test/AnchorMapGeneratorTest.java
+++ AnchorParser/test/AnchorMapGeneratorTest.java
@@ -1,5 +1,10 @@
 import static org.junit.Assert.*;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
+
 import org.junit.Test;
 
 
@@ -7,8 +12,29 @@ public class AnchorMapGeneratorTest {
 
 	@Test
 	public void generateURIKeyMaptest() {
-		AnchorMapGenerator mapGenerator = new AnchorMapGenerator();
-		fail("Not yet implemented");
+		HashMap<String, LinkedList<String>> URIKeyMap;
+		try{
+			URIKeyMap = AnchorMapGenerator.generateURIKeyMap("data/anchorMapGenerator_test.txt");
+			
+			assertEquals("Number of key-value pairs is 5.", 5, URIKeyMap.size());
+			assertEquals("key 'link02' has 3 entries.", 3, URIKeyMap.get("link02").size());
+			
+			AnchorMapGenerator.saveURIKeyMapToFile(URIKeyMap, "data/URIKeyMapSave.ukm");
+			URIKeyMap = AnchorMapGenerator.loadURIKeyMapFromFile("data/URIKeyMapSave.ukm");
+			
+			assertEquals("Number of key-value pairs is 5 in loaded map.", 5, URIKeyMap.size());
+			assertEquals("key 'link02' has 3 entries in loaded map.", 3, URIKeyMap.get("link02").size());
+			
+		}catch(FileNotFoundException e){
+			fail("File not found exception");
+			e.printStackTrace();
+		}catch(IOException e){
+			fail("IO exception");
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			fail("ClassNotFoundException");
+			e.printStackTrace();
+		}
 	}
 
 }
