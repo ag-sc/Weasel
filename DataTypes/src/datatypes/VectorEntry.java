0b5fb17aaaa86adeb8a05769425f23fa6feaf67a
Felix Tristram <felix.tristram@googlemail.com>
2014-12-16 11:30:52 +0100

    Starting vector map overhaul.

---
+++ DataTypes/src/datatypes/VectorEntry.java
@@ -0,0 +1,13 @@
+package datatypes;
+
+import java.io.Serializable;
+
+public final class VectorEntry implements Serializable{
+	private static final long serialVersionUID = -24055471855674856L;
+	
+	public static final int semSigVector[] = new int[100];
+	public static final int semSigCount[] = new int[100];
+	
+	public static final int tfVector[] = new int[100];
+	public static final float tfScore[] = new float[100];
+}
