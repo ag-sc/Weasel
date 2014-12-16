0bf000e2adbd039098f1961b799a4978cb17bce7
Felix Tristram <felix.tristram@googlemail.com>
2014-12-16 14:19:28 +0100

    VectorMap rework is done. It is untested though.

--- DataTypes/src/datatypes/VectorEntry.java
+++ DataTypes/src/datatypes/VectorEntry.java
@@ -3,11 +3,18 @@ package datatypes;
 import java.io.Serializable;
 
 public final class VectorEntry implements Serializable{
-	private static final long serialVersionUID = -24055471855674856L;
+	private static final long serialVersionUID = -7333749930975596124L;
+
+	public final double pagerank = 0.0;
 	
-	public static final int semSigVector[] = new int[100];
-	public static final int semSigCount[] = new int[100];
+	public final int semSigVector[] = new int[100];
+	public final int semSigCount[] = new int[100];
 	
-	public static final int tfVector[] = new int[100];
-	public static final float tfScore[] = new float[100];
+	public final int tfVector[] = new int[100];
+	public final float tfScore[] = new float[100];
+	
+	public VectorEntry(){
+		for(int i = 0; i < semSigVector.length; i++) semSigVector[i] = -1; // valid entries are >= 0
+		for(int i = 0; i < tfVector.length; i++) tfVector[i] = -1;
+	}
 }
--- H2Sandbox/src/SignaturemapSandbox.java
+++ H2Sandbox/src/SignaturemapSandbox.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
@@ -8,27 +10,30 @@ import java.io.ObjectOutputStream;
 import java.sql.SQLException;
 import java.text.Normalizer;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.List;
+import java.util.Map.Entry;
+
 import stopwatch.Stopwatch;
 import tfidf.DocumentFrequency;
 import tfidf.TFIDF;
 import databaseConnectors.H2Connector;
 import datatypes.TFIDFResult;
-import datatypes.Tuple;
+import datatypes.VectorEntry;
 
 
 public class SignaturemapSandbox {
 
-//	final static String dfPath = "../../data/Wikipedia Abstracts/documentFrequency";
-//	final static String abstractPath = "../../data/Wikipedia Abstracts/test_abstracts_cleaned_correct.txt";
-//	final static String dbPathH2 = "E:/Master Project/data/H2/AnchorsPlusPagelinks/h2_anchors_pagelinks";
-//	final static String outputPath = "../../data/Wikipedia Abstracts/bigmap/bigmap_";
+	final static String dfPath = "../../data/Wikipedia Abstracts/documentFrequency";
+	final static String abstractPath = "../../data/Wikipedia Abstracts/test_abstracts_cleaned_correct.txt";
+	final static String dbPathH2 = "E:/Master Project/data/H2/AnchorsPlusPagelinks/h2_anchors_pagelinks";
+	final static String semsigPath = "../../data/semsig.txt";
 	
-	final static String dfPath = "documentFrequency";
-	final static String abstractPath = "abstracts_cleaned_correct.txt";
-	final static String dbPathH2 = "/media/data/shared/ftristram/pageLinks/H2 Anchors/h2_anchors_pagelinks";
-	final static String outputPath = "/media/data/shared/ftristram/bigmap/map/bigmap_";
+//	final static String dfPath = "documentFrequency";
+//	final static String abstractPath = "abstracts_cleaned_correct.txt";
+//	final static String dbPathH2 = "/media/data/shared/ftristram/pageLinks/H2 Anchors/h2_anchors_pagelinks";
+//	final static String semsigPath = "semsig.txt";
 	
 	final static int initialMapSize = 200000;
 	
@@ -51,41 +56,37 @@ public class SignaturemapSandbox {
 		String line;
 		int counter = 0;
 		
-		// create fingerprint map
-		HashMap<Integer, Tuple<ArrayList<Integer>, HashMap<Integer, Float>>> map = new HashMap<Integer, Tuple<ArrayList<Integer>, HashMap<Integer, Float>>>(initialMapSize);
+		// create vector map
+		HashMap<Integer, VectorEntry> vectorMap = new HashMap<Integer, VectorEntry>(initialMapSize);
 		
 		// Set up H2 Connector
 		String sql = "select entitySinkIDList from EntityToEntity where EntitySourceID is (?)";
 		H2Connector entityDB = new H2Connector(dbPathH2, "sa", "", sql, false);
 		
 		System.out.println("Starting Loop");
-		int partCounter = 1;
 		Stopwatch sw = new Stopwatch(Stopwatch.UNIT.SECONDS);
 		while((line = br.readLine()) != null){
 			counter++;
 			
-			if(counter % 10000 == 0){
+			if(counter % 100000 == 0){
 				System.out.println(counter + " - " + sw.stop() + " s");
 				sw.start();
 			}
 			
-			if(counter % 100000 == 0){
-				ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(outputPath + partCounter));
-				out.writeObject(map);
-				out.close();
-				System.out.println("Wrote 'bigmap_"+partCounter + "' to file - " + sw.stop() + " s");
-				sw.start();
-				partCounter++;
-				map = new HashMap<Integer, Tuple<ArrayList<Integer>, HashMap<Integer, Float>>>(initialMapSize);
-			}
+//			if(counter % 100000 == 0){
+//				ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(outputPath + partCounter));
+//				out.writeObject(vectorMap);
+//				out.close();
+//				System.out.println("Wrote 'bigmap_"+partCounter + "' to file - " + sw.stop() + " s");
+//				sw.start();
+//				partCounter++;
+//				vectorMap = new HashMap<Integer, VectorEntry>(initialMapSize);
+//			}
 			
 			// Get EntityID and Semantic Signature
 			//String title = StringConverter.convert(br.readLine().replace(" ", "_"), "UTF-8");
 			String title = br.readLine().replace(" ", "_");
 			line = br.readLine().toLowerCase();
-			
-			//if(counter > 370 && counter < 380) System.out.println(title);
-			
 			Integer id = entityDB.resolveName(title);
 			if(id == null){
 				String normalized = Normalizer.normalize(title, Normalizer.Form.NFD);
@@ -96,31 +97,85 @@ public class SignaturemapSandbox {
 					continue;
 				}
 			}
-			LinkedList<String> semSigTemp = entityDB.getFragmentTargets(id.toString());
-			if(semSigTemp.size() < 2) continue;
-			ArrayList<Integer> semSig = new ArrayList<Integer>();
-			for(String s: semSigTemp) semSig.add(Integer.parseInt(s));
 			
 			//System.out.println("Start on TFIDF");
 			// calculate TF/IDF
-			LinkedList<TFIDFResult> resultList = new LinkedList<TFIDFResult>();			
+			
+			List<TFIDFResult> resultList = new ArrayList<TFIDFResult>();			
 			resultList = TFIDF.compute(line, df);
+			Collections.sort(resultList);
+			Collections.reverse(resultList);
 			
-			HashMap<Integer, Float> top100TFIDF = new HashMap<Integer, Float>();
-			
+			VectorEntry vEntry = new VectorEntry();
 			for(int i = 0; i < 100 && i < resultList.size(); i++){
-				top100TFIDF.put(df.getWordID(resultList.get(i).token), resultList.get(i).tfidf);
+				vEntry.tfVector[i] = df.getWordID(resultList.get(i).token);
+				vEntry.tfScore[i]  = resultList.get(i).tfidf;
 			}
-			
-			map.put(id, new Tuple<ArrayList<Integer>, HashMap<Integer, Float>>(semSig, top100TFIDF));
+			vectorMap.put(id, vEntry);
 
 			
 		}
 		br.close();
-		entityDB.close();
-		
-		System.out.println("All done. #Abstracts: " + counter + " - time taken: " + sw2.stop() + " hours");
 
+		
+		System.out.println("Done with TF/IDF. #Abstracts: " + counter);
+		
+		System.out.println("Start on Semantic Signature");
+		System.err.println("Start on Semantic Signature");
+		counter = 0;
+		sw.start();
+		br = new BufferedReader(new InputStreamReader(new FileInputStream(semsigPath), "UTF8"));
+		while((line = br.readLine()) != null){
+			counter++;
+			if(counter % 100000 == 0){
+				System.out.println(counter + " semsig entries:\t" + sw.stop() + " s");
+				sw.start();
+			}
+			int id = entityDB.resolveName(line);
+			if(!vectorMap.containsKey(id)){
+				System.err.println(line + " not in vectorMap");
+				while(line != null && !line.equals("")) line = br.readLine(); // skip entry
+				continue;
+			}else{
+				VectorEntry entry = vectorMap.get(id);
+				for(int i = 0; i < 100; i++){
+					line = br.readLine();
+					if(line.equals("")) break;
+					String lineArray[] = line.split("\t");
+					entry.semSigVector[i] = entityDB.resolveName(lineArray[0]);
+					entry.semSigCount[i]  = Integer.parseInt(lineArray[1]);
+				}
+				while(!line.equals("")) line = br.readLine();
+			}
+		}
+		
+		br.close();
+		entityDB.close();
+		sw.start();
+		System.out.println("Done with semsig."  + " - time taken: " + sw2.stop() + " hours");
+		
+		System.out.println("Write map to binary file");
+		ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("../../data/vectorMap"));
+		out.writeObject(vectorMap);
+		out.close();
+		System.out.println(("Write to binary file: Done! Write map to text file."));
+		BufferedWriter fw = new BufferedWriter(new FileWriter("../../data/vectorMap.txt"));
+		for(Entry<Integer, VectorEntry> entry: vectorMap.entrySet()){
+			VectorEntry vEntry = entry.getValue();
+			
+			fw.write(entry.getKey() + "\t" + vEntry.pagerank + "\n");
+			for(Integer i: vEntry.semSigVector) fw.write(i + "\t");
+			fw.write("\n");
+			for(Integer i: vEntry.semSigCount) fw.write(i + "\t");
+			fw.write("\n");
+			for(Integer i: vEntry.tfVector) fw.write(i + "\t");
+			fw.write("\n");
+			for(Float f: vEntry.tfScore) fw.write(f + "\t");
+			fw.write("\n");
+			fw.write("\n");
+		}
+		fw.close();
+		System.out.println("All done! Time taken for file writing: " + sw.stop() + " s");
 	}
 
 }
