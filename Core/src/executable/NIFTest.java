d48901f5c3708d6c95a387f811820eba1c0893d1
Felix Tristram <felix.tristram@googlemail.com>
2015-06-24 16:58:41 +0200

    - Continued working on NIF integration.

---
+++ Core/src/executable/NIFTest.java
@@ -0,0 +1,22 @@
+package executable;
+
+import nif.NIFAdapter;
+import iniloader.IniLoader;
+import datasetEvaluator.DatasetEvaluatorSandbox;
+
+public class NIFTest {
+
+	public static void main(String[] args) {
+		String filepath = "../config.ini";
+		if (args.length == 1)
+			filepath = args[0];
+		System.out.println("Using config file: " + filepath);
+		IniLoader iniLoader = new IniLoader();
+		iniLoader.parse(filepath);
+
+		DatasetEvaluatorSandbox sandbox = new DatasetEvaluatorSandbox();
+		NIFAdapter adapter = new NIFAdapter("E:/Master Project/data/NIF/nifTest.ttl", sandbox);
+		adapter.linkModel();
+	}
+
+}
--- Core/src/nif/NIFAdapter.java
+++ Core/src/nif/NIFAdapter.java
@@ -1,28 +1,37 @@
 package nif;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.SimpleSelector;
 import com.hp.hpl.jena.rdf.model.Statement;
 import com.hp.hpl.jena.rdf.model.StmtIterator;
 import com.hp.hpl.jena.util.FileManager;
 
+import datasetEvaluator.DatasetEvaluatorSandbox;
 import datatypes.annotatedSentence.AnnotatedSentence;
 
 public class NIFAdapter {
 
 	Model model;
+	DatasetEvaluatorSandbox evaluator;
 
-	public NIFAdapter() {
+	public NIFAdapter(String filePath, DatasetEvaluatorSandbox evaluator) {
 		model = ModelFactory.createDefaultModel();
+		loadModelFromFile(filePath);
+		this.evaluator = evaluator;
 	}
 
-	public void loadModelFromFile(String filePath) {
+	private void loadModelFromFile(String filePath) {
 		// create an empty model
-		Model model = ModelFactory.createDefaultModel();
+		model = ModelFactory.createDefaultModel();
 		// use the FileManager to find the input file
 		InputStream in = FileManager.get().open(filePath);
 		if (in == null) {
@@ -32,16 +41,26 @@ public class NIFAdapter {
 		model.read(in, null, "TTL");
 	}
 	
-	public List<AnnotatedSentence> convertModelToAS(){
-		List<AnnotatedSentence> sentenceList = new LinkedList<AnnotatedSentence>();
-		
-		StmtIterator iter = model.listStatements(new IsStringSelector());
+	public void linkModel(){
+		StmtIterator iter = model.listStatements(new SimpleSelector(null, null, (RDFNode) NIF_SchemaGen.Context));
 		while (iter.hasNext()) {
 			Statement stmt = iter.nextStatement(); // get next statement
+			Resource originResource = stmt.getSubject();
+			
+			StmtIterator tmpIter = model.listStatements(new SimpleSelector(originResource, NIF_SchemaGen.isString,(RDFNode) null));
+			String originSentence = tmpIter.nextStatement().getObject().asLiteral().toString().split("\\^\\^")[0];
+			
+			System.out.println("Origin Sentence: " + originSentence);
+			AnnotatedSentence as = new AnnotatedSentence();
+			
+			List<Resource> wordList = new ArrayList<Resource>();
+			tmpIter = model.listStatements(new SimpleSelector(null, NIF_SchemaGen.referenceContext,(RDFNode) originResource));
+			while (tmpIter.hasNext()) {
+				System.out.println(tmpIter.nextStatement());
+			}
 		}
-		
-		return sentenceList;
 	}
+	
 }
 
 
