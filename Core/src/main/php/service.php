5e53be78edf4dbacf2aa76cdacc6bc73d99c75ca
Felix Tristram <felix.tristram@googlemail.com>
2015-07-14 15:03:15 +0200

    - created a small php script to query the spring boot server.

---
+++ Core/src/main/php/service.php
@@ -0,0 +1,73 @@
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
+
+<?php
+$message = "Enter text here...";
+if(isset($_POST['inputArea'])){
+	$message = $_POST['inputArea'];
+}	
+?>
+
+<html>
+    <head>
+        <title>Veasel Webservice</title>
+    </head>
+    <body>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<h2>Veasel webservice test</h2>
+		
+		
+        <form action="service.php" method="POST">
+            <textarea rows="10" cols="80" name="inputArea"><?= $message ?></textarea> 
+			<input type="submit">
+        </form>
+		
+    </body>
+</html>
+
+<?php
+	// Validate input
+	$result = "";
+	$timeout = 60;
+	if(!isset($_POST['inputArea'])){
+		echo "Timeout is currently set to " . $timeout . " seconds.";
+		exit(-1);
+	}
+
+	$input = $_POST['inputArea'];
+
+	$url = 'http://localhost:8080/formatted-output';
+	$data = array('input' => $input);
+
+	// use key 'http' even if you send the request to https://...
+	$options = array(
+		'http' => array(
+			'header'  => "Content-type: application/x-www-form-urlencoded",
+			'method'  => 'POST',
+			'content' => http_build_query($data),
+			'timeout' => $timeout
+		),
+	);
+	$context  = stream_context_create($options);
+	$result = file_get_contents($url, false, $context);
+
+	echo(nl2br($result));
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Core/src/springboot/backend/InputController.java
+++ Core/src/springboot/backend/InputController.java
@@ -2,9 +2,15 @@ package springboot.backend;
 
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
+import nif.ITSRDF_SchemaGen;
 import nif.ModelAdapter;
 import nif.NIFAdapter;
+import nif.NIF_SchemaGen;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,8 +18,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.SimpleSelector;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
 
 import datasetEvaluator.DatasetEvaluatorSandbox;
+import datatypes.Tuple;
 import datatypes.configuration.Config;
 import entityLinker.InputStringHandler;
 
@@ -23,6 +35,7 @@ public class InputController {
 	DatasetEvaluatorSandbox sandbox;
 	InputStringHandler handler;
 	ModelAdapter adapter;
+	String wikiString = "https://en.wikipedia.org/wiki/";
 
 	public InputController() throws ClassCastException, ClassNotFoundException, IOException {
 		System.out.println("Inputcontroller created");
@@ -34,13 +47,75 @@ public class InputController {
 	@RequestMapping(method = RequestMethod.POST, value = "/veasel-english")
 	public String handleInputEnglish(@RequestParam(value = "input") String input) {
 		// error handling
-		if (input == null)
-			return "Empty input!";
+		if (input == null) return "Empty input!";
 
+		return handleSentence(input);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/formatted-output")
+	public String handleInputFormattedOutput(String input){
+		// error handling
+		if (input == null) return "Empty input!";
+		Model model = handleSentenceGetModel(input);
+		StringBuilder sb = new StringBuilder();
+		
+		StmtIterator resultIter = model.listStatements(new SimpleSelector(null, null, (RDFNode) NIF_SchemaGen.Context));
+		while (resultIter.hasNext()) {
+			Statement stmt = resultIter.nextStatement(); // get next statement
+			Resource originResource = stmt.getSubject();
+			
+			List<Resource> resourceResults = new ArrayList<Resource>();
+			
+			StmtIterator tokenIter = model.listStatements(new SimpleSelector(null, NIF_SchemaGen.referenceContext, (RDFNode) originResource));
+			while (tokenIter.hasNext()) {
+				Resource tokenResource= tokenIter.next().getSubject();
+				Statement entityStatement = tokenResource.getProperty(ITSRDF_SchemaGen.taIdentRef);
+				if(entityStatement != null){
+					resourceResults.add(tokenResource);
+				}
+			}
+			
+			// sort resources by beginIndex
+			Collections.sort(resourceResults, new Comparator<Resource>() {
+				@Override
+				public int compare(Resource o1, Resource o2) {
+					int beginIndex1 = o1.getProperty(NIF_SchemaGen.beginIndex).getInt();
+					int beginIndex2 = o2.getProperty(NIF_SchemaGen.beginIndex).getInt();
+					return beginIndex1 - beginIndex2;
+				}
+			});
+			
+			// replace entities in string by formatted result
+			String sentence = originResource.getProperty(NIF_SchemaGen.isString).getObject().toString();
+			int offset = 0;
+			for(Resource resource: resourceResults){
+				int beginIndex = resource.getProperty(NIF_SchemaGen.beginIndex).getInt();
+				int endIndex = resource.getProperty(NIF_SchemaGen.endIndex).getInt();
+				String entity = resource.getProperty(ITSRDF_SchemaGen.taIdentRef).getString();
+				String anchorOf = resource.getProperty(NIF_SchemaGen.anchorOf).getString();
+				String url = wikiString + entity;
+				String formattedEntity = "<a href=\"" + url + "\">" + anchorOf + "</a>";
+				
+				int lengthBefore = sentence.length();
+				sentence = sentence.substring(0, beginIndex + offset) + formattedEntity + sentence.substring(endIndex + offset);
+				offset += sentence.length() - lengthBefore;
+			}
+			sb.append(sentence + "\n");
+		}
+		
+		return sb.toString();
+	}
+	
+	private Model handleSentenceGetModel(String inputSentence){
 		handler.resetModel();
-		handler.handleString(input);
+		handler.handleString(inputSentence);
 		Model model = handler.getModel();
 		adapter.linkModel(model);
+		return model;
+	}
+	
+	private String handleSentence(String inputSentence){
+		Model model = handleSentenceGetModel(inputSentence);
 		StringWriter stringWriter = new StringWriter();
 		model.write(stringWriter, "Turtle");
 		String output = stringWriter.toString();
